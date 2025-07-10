49833947a9d783528e4b5987f3a1327c1bbfbd12
Charles Chow <you@example.com>
2025-07-10 22:29:58 +0800

    feat: navigation & layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+      <v-navigation-drawer v-model="drawer">
+        <!--  -->
+      </v-navigation-drawer>
+
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-app-bar-title>Application</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+          >
+        </v-btn>
+      </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
@@ -7,5 +26,8 @@
 </template>
 
 <script setup>
-  //
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+  const links = ['Dashboard', 'Profile', 'Settings']
 </script>
